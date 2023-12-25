c951690fb6fe30adbac1f09cdbf5f239d93b3e79
jaycehan <jaycehan@tencent.com>
2023-12-26 01:33:28 +0800

    Delete useless code

--- JCImage/Module/OpenGLES/JCProgramProvider.m
+++ JCImage/Module/OpenGLES/JCProgramProvider.m
@@ -8,26 +8,6 @@
 #import <OpenGLES/ES3/gl.h>
 #import "JCProgramProvider.h"
 
-static char *COMMON_VERTEX_SHADER =
-      "attribute vec4 position;                   \n"
-      "attribute vec2 texcoord;                   \n"
-      "varying vec2 v_texcoord;                   \n"
-      "                                            \n"
-      "void main(void)                              \n"
-      "{                                              \n"
-      "   gl_Position = position;                 \n"
-      "   v_texcoord = texcoord;                  \n"
-      "}                                              \n";
-
-static char* COMMON_FRAG_SHADER =
-      "precision highp float;                                       \n"
-      "varying highp vec2 v_texcoord;                               \n"
-      "uniform sampler2D texSampler;                                \n"
-      "                                                               \n"
-      "void main() {                                                 \n"
-      "    gl_FragColor = texture2D(texSampler, v_texcoord);      \n"
-      "}                                                                                                                                       \n";
-
 @implementation JCProgramProvider
 
 + (void)shader:(GLuint *)shader type:(GLenum)type filePath:(NSString *)filePath {
