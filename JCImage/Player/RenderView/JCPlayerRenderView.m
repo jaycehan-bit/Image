ca71ecac96bede5b7f0f5570b081681e3582f6cf
jaycehan <jaycehan@tencent.com>
2024-02-05 14:54:58 +0800

    feat: 继续学习Player

--- JCImage/Player/RenderView/JCPlayerRenderView.m
+++ JCImage/Player/RenderView/JCPlayerRenderView.m
@@ -9,12 +9,5 @@
 
 @implementation JCPlayerRenderView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
